c48fff740e2f919746925146e6ca73b02c27c5ba
trandat812003 <datnguyentran8@gmail.com>
2024-11-28 15:55:55 +0700

    test

--- libs/utils/get_model.py
+++ libs/utils/get_model.py
@@ -4,7 +4,7 @@ from modules.my_model import MyModel
 from libs.config import BlenderbotConfig
 
 
-def get_model(checkpoint: str=None):
+def get_model(checkpoint: str=None, device=torch.device('cpu')):
     model = MyModel.from_pretrained(BlenderbotConfig.PRETRAIN_MODEL)
     if BlenderbotConfig.CUSTOM_CONFIG_PATH is not None:
         model = MyModel(AutoConfig.from_pretrained(BlenderbotConfig.CUSTOM_CONFIG_PATH))
@@ -12,6 +12,6 @@ def get_model(checkpoint: str=None):
     setattr(model.config, 'gradient_checkpointing', BlenderbotConfig.GRADIENT_CHECKPOINTING)
     
     if checkpoint is not None:
-        model.load_state_dict(torch.load(checkpoint, map_location=torch.device('cpu')))
+        model.load_state_dict(torch.load(checkpoint, map_location=device))
     
     return model
