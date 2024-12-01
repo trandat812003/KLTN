31a134e7d3001306362bb3084722eef069a2d1b4
trandat812003 <datnguyentran8@gmail.com>
2024-12-01 22:16:17 +0700

    test"

--- modules/my_datamodule.py
+++ modules/my_datamodule.py
@@ -6,6 +6,10 @@ from libs.config import Config
 from libs.dataset import BaseDataset, MIDataset, ESConvDataset
 from libs.utils.input_feature import InputFeature
 from multiprocessing import cpu_count
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from typing import List
+from libs.config import Config
 
 
 class MyDataModule(L.LightningDataModule):
@@ -39,10 +43,7 @@ class MyDataModule(L.LightningDataModule):
             self.train_dataset,
             batch_size=self.BATCH_SIZE, 
             num_workers=self.num_workers,
-            collate_fn=partial(
-                    InputFeature.collate, 
-                    tokenizer=self.tokenizer
-                )
+            collate_fn=partial(MyDataModule.collate, tokenizer=self.tokenizer)
         )
 
     def val_dataloader(self):
@@ -50,10 +51,7 @@ class MyDataModule(L.LightningDataModule):
             self.dev_dataset,
             batch_size=self.BATCH_SIZE, 
             num_workers=self.num_workers,
-            collate_fn=partial(
-                    InputFeature.collate, 
-                    tokenizer=self.tokenizer
-                )
+            collate_fn=partial(MyDataModule.collate, tokenizer=self.tokenizer)
         )
 
     def test_dataloader(self):
@@ -61,10 +59,7 @@ class MyDataModule(L.LightningDataModule):
             self.test_dataset,
             batch_size=self.BATCH_SIZE, 
             num_workers=self.num_workers,
-            collate_fn=partial(
-                    InputFeature.collate, 
-                    tokenizer=self.tokenizer
-                )
+            collate_fn=partial(MyDataModule.collate, tokenizer=self.tokenizer, is_test=True)
         )
 
     def predict_dataloader(self):
@@ -72,8 +67,62 @@ class MyDataModule(L.LightningDataModule):
             self.test_dataset,
             batch_size=self.BATCH_SIZE, 
             num_workers=self.num_workers,
-            collate_fn=partial(
-                    InputFeature.collate, 
-                    tokenizer=self.tokenizer
-                )
+            collate_fn=partial(MyDataModule.collate, tokenizer=self.tokenizer, is_test=True)
         )
+    
+    @staticmethod
+    def collate(features: List[InputFeature], tokenizer: PreTrainedTokenizer, is_test=False):
+        pad = tokenizer.pad_token_id
+        if pad is None:
+            pad = tokenizer.eos_token_id
+            assert pad is not None, 'either pad_token_id or eos_token_id should be provided'
+        
+        input_ids = pad_sequence(
+            [torch.tensor(f.input_ids, dtype=torch.long) for f in features],
+            batch_first=True, 
+            padding_value=pad
+        )
+        attention_mask = pad_sequence(
+            [torch.tensor([1.] * f.input_length, dtype=torch.float) for f in features],
+            batch_first=True, 
+            padding_value=0.
+        )
+        
+        if is_test:
+            decoder_input_ids = torch.tensor([[f.decoder_input_ids[0]] for f in features], dtype=torch.long)
+            labels = None
+        else:
+            decoder_input_ids = pad_sequence(
+                [torch.tensor(f.decoder_input_ids, dtype=torch.long) for f in features],
+                batch_first=True, 
+                padding_value=pad
+            )
+            labels = pad_sequence(
+                [torch.tensor(f.labels, dtype=torch.long) for f in features],
+                batch_first=True, 
+                padding_value=-100
+            )
+        
+        if Config.DATA_NAME == 'esconv':
+            strat_id = torch.tensor([f.labels[0] for f in features], dtype=torch.long) - len(tokenizer) + 8
+        elif Config.DATA_NAME == 'mi':
+            strat_id = torch.tensor([f.labels[0] for f in features], dtype=torch.long) - len(tokenizer) + 10
+        
+        if Config.KNOWLEDGE_NAME == 'basic':
+            strat_id += 5
+        elif Config.KNOWLEDGE_NAME == 'bm25':
+            strat_id += 1
+        elif Config.KNOWLEDGE_NAME == 'oracle':
+            strat_id += 6
+        elif Config.KNOWLEDGE_NAME in ['sbert','graph']:
+            strat_id += 8
+        
+        res = {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'decoder_input_ids': decoder_input_ids,
+            'labels': labels,
+            'strat_id': strat_id,
+        }
+        
+        return res
