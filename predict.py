1b82708ea618c1d94cca46084141b6e2bf7e1593
trandat812003 <datnguyentran8@gmail.com>
2024-12-25 23:54:02 +0700

    test

--- predict.py
+++ predict.py
@@ -8,7 +8,6 @@ from libs.dataset import BaseDataset, MIDataset, ESConvDataset
 from libs.config import Config
 from libs.utils.get_tokenizer import get_tokenizer
 from libs.utils.get_model import get_model
-from libs.utils.get_checkpoint import get_checkpoints
 from libs.utils.utils import cut_seq_to_eos, norm
 from libs.metric.metrics import Metric
 
