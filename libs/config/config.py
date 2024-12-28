60ae2f2add426d16243f817c39923ef3026646f6
trandat812003 <datnguyentran8@gmail.com>
2024-12-28 23:47:55 +0700

    test

--- libs/config/config.py
+++ libs/config/config.py
@@ -6,13 +6,58 @@ load_dotenv('./config/.env')
 
 
 class Config:
-    DATA_NAME = os.getenv("DATA_NAME")
-    KNOWLEDGE_NAME = os.getenv("KNOWLEDGE_NAME")
-    BASELINE = os.getenv("BASELINE")
-    DATA_DIR = os.getenv("DATA_DIR")
-    BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
-    GRADIENT_ACCUMULATION_STEPS = int(os.getenv("GRADIENT_ACCUMULATION_STEPS"))
+    _DATA_NAME = os.getenv("DATA_NAME")
+    _KNOWLEDGE_NAME = os.getenv("KNOWLEDGE_NAME")
+    _BASELINE = os.getenv("BASELINE")
+    _DATA_DIR = os.getenv("DATA_DIR")
+    _NUM_EPOCHS = int(os.getenv("NUM_EPOCHS"))
+    _BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
+    _GRADIENT_ACCUMULATION_STEPS = int(os.getenv("GRADIENT_ACCUMULATION_STEPS"))
+    _MAX_INPUT_LENGTH = int(os.getenv("MAX_INPUT_LENGTH"))
+    _MAX_DECODER_INPUT_LENGTH = int(os.getenv("MAX_DECODER_INPUT_LENGTH"))
     _WANDB_NAME = None
+
+    @classmethod
+    @property
+    def DATA_NAME(cls):
+        if cls._DATA_NAME is None:
+            cls._DATA_NAME = 'esconv'
+        return cls._DATA_NAME
+    
+    @classmethod
+    @property
+    def KNOWLEDGE_NAME(cls):
+        if cls._KNOWLEDGE_NAME is None:
+            cls._KNOWLEDGE_NAME = 'sbert'
+        return cls._KNOWLEDGE_NAME
+    
+    @classmethod
+    @property
+    def BASELINE(cls):
+        if cls._BASELINE is None:
+            cls._BASELINE = 'kemi'
+        return cls._BASELINE
+    
+    @classmethod
+    @property
+    def DATA_DIR(cls):
+        if cls._DATA_DIR is None:
+            cls._DATA_DIR = 'dataset/'
+        return cls._DATA_DIR
+    
+    @classmethod
+    @property
+    def BATCH_SIZE(cls):
+        if cls._BATCH_SIZE is None:
+            cls._BATCH_SIZE = 25
+        return cls._BATCH_SIZE
+    
+    @classmethod
+    @property
+    def GRADIENT_ACCUMULATION_STEPS(cls):
+        if cls._GRADIENT_ACCUMULATION_STEPS is None:
+            cls._GRADIENT_ACCUMULATION_STEPS = 1.0
+        return cls._GRADIENT_ACCUMULATION_STEPS
 
     @classmethod
     @property
